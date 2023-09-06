988db3c6c3bc20740a0c8d8b10c016e5a77dee70
Firli Ilhami <firli.ilhami@gmail.com>
2023-09-06 18:52:31 +0700

    add routing

--- app/router/router.go
+++ app/router/router.go
@@ -4,6 +4,9 @@ import (
 	classData "immersive_project/klp3/features/classes/data"
 	classHandler "immersive_project/klp3/features/classes/handler"
 	classService "immersive_project/klp3/features/classes/service"
+	usersData "immersive_project/klp3/features/users/data"
+	usersHandler "immersive_project/klp3/features/users/handler"
+	usersService "immersive_project/klp3/features/users/service"
 
 	menteeLogData "immersive_project/klp3/features/menteelogs/data"
 	menteeLogHandler "immersive_project/klp3/features/menteelogs/handler"
@@ -13,22 +16,32 @@ import (
 	"gorm.io/gorm"
 )
 
-func InitRouter(db *gorm.DB,c *echo.Echo){
-	CData:=classData.New(db)
-	CService:=classService.New(CData)
-	CHandler:=classHandler.New(CService)
+func InitRouter(db *gorm.DB, c *echo.Echo) {
+	CData := classData.New(db)
+	CService := classService.New(CData)
+	CHandler := classHandler.New(CService)
 
-	c.POST("/classes",CHandler.Add)
-	c.GET("/classes",CHandler.GetAll)
-	c.PUT("/classes/:class_id",CHandler.Edit)
-	c.GET("/classes/:class_id",CHandler.GetById)
-	c.DELETE("/classes/:class_id",CHandler.Delete)
+	c.POST("/classes", CHandler.Add)
+	c.GET("/classes", CHandler.GetAll)
+	c.PUT("/classes/:class_id", CHandler.Edit)
+	c.GET("/classes/:class_id", CHandler.GetById)
+	c.DELETE("/classes/:class_id", CHandler.Delete)
 
-	MLData:=menteeLogData.New(db)
-	MLService:=menteeLogService.New(MLData)
-	MLHandler:=menteeLogHandler.New(MLService)
-	c.POST("/mentees/:mentee_id/logs",MLHandler.Add)
-	c.GET("/mentees/:mentee_id/logs",MLHandler.Get)
-	c.PUT("/logs/:log_id",MLHandler.Edit)
-	c.DELETE("/logs/:log_id",MLHandler.Delete)
+	MLData := menteeLogData.New(db)
+	MLService := menteeLogService.New(MLData)
+	MLHandler := menteeLogHandler.New(MLService)
+	c.POST("/mentees/:mentee_id/logs", MLHandler.Add)
+	c.GET("/mentees/:mentee_id/logs", MLHandler.Get)
+	c.PUT("/logs/:log_id", MLHandler.Edit)
+	c.DELETE("/logs/:log_id", MLHandler.Delete)
+
+	userData := usersData.New(db)
+	userService := usersService.New(userData)
+	userHandler := usersHandler.New(userService)
+
+	c.POST("/login", userHandler.UserLogin)
+	c.GET("/users/", userHandler.FindAll)
+	c.GET("/users/:user_id", userHandler.FindById)
+	c.PUT("/users/:user_id", userHandler.Update)
+	c.DELETE("/users/:user_id", userHandler.Delete)
 }
